2d6316a4bfac823bb5a5fc48094db96dcb0769a9
Staudiz <s.staudenmayer@outlook.com>
2026-01-17 18:05:17 +0100

    feat: added otel for web
    Refs:

---
+++ web/otel.ts
@@ -0,0 +1,35 @@
+import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base';
+import { WebTracerProvider } from '@opentelemetry/sdk-trace-web';
+import { ZoneContextManager } from '@opentelemetry/context-zone';
+import { registerInstrumentations } from '@opentelemetry/instrumentation';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
+import { resourceFromAttributes } from '@opentelemetry/resources';
+import {
+  ATTR_SERVICE_NAME,
+  ATTR_SERVICE_VERSION,
+} from '@opentelemetry/semantic-conventions';
+
+const resource = resourceFromAttributes({
+  [ATTR_SERVICE_NAME]: 'jira-dash',
+  [ATTR_SERVICE_VERSION]: '1.0.0',
+});
+
+const provider = new WebTracerProvider({
+  resource,
+  spanProcessors: [new SimpleSpanProcessor(new OTLPTraceExporter({
+  }))],
+});
+
+provider.register({
+  // Changing default contextManager to use ZoneContextManager - supports asynchronous operations - optional
+  contextManager: new ZoneContextManager(),
+});
+
+// Registering instrumentations
+registerInstrumentations({
+  instrumentations: [
+    getWebAutoInstrumentations({})
+  ],
+});
+
